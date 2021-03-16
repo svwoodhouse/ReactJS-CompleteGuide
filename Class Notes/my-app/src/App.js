1f8008a744137aea6db0e5cb77f464eb7ef22bf1
Sydnee Woodhouse <svwoodhouse@gmail.com>
2021-03-15 22:58:08 -0400

    Outputting Lists

--- Class Notes/my-app/src/App.js
+++ Class Notes/my-app/src/App.js
@@ -49,24 +49,28 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null
+
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          {this.state.persons.map(person => {
+              return <Person 
+                name = {person.name}
+                age = {person.age} />
+          })};
+      </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button 
           onClick={this.togglePersonHandler}
-          style={style}>Switch Name</button>
-          {this.state.showPerson ?
-            <div>
-              <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, "Max!")}
-                changed={this.nameChangeHandler}>My Hobbies: Racing</Person>
-              <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-            </div> : null
-          }
+          style={style}>Toggle Persons</button>
+        {persons}
       </div>
     )
   }
