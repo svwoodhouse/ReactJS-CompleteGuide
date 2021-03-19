1cb89776ca15444b6aa90d63abba7e967d1fb972
Sydnee Woodhouse <svwoodhouse@gmail.com>
2021-03-19 00:20:17 -0400

    few changes

--- Class Notes/my-app/src/App.js
+++ Class Notes/my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -48,7 +49,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null
@@ -67,6 +72,10 @@ class App extends Component {
       </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -89,4 +98,4 @@ class App extends Component {
     )
   }
 }
-export default App;
+export default Radium(App);
