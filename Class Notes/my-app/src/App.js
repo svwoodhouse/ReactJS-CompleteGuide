c15282c8166fbd49d4fe9d5721b8d0d26c6bcd47
Sydnee Woodhouse <svwoodhouse@gmail.com>
2021-03-18 23:45:13 -0400

    Dynamically assign styles example

--- Class Notes/my-app/src/App.js
+++ Class Notes/my-app/src/App.js
@@ -43,7 +43,8 @@ class App extends Component {
 
   render () {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -65,12 +66,21 @@ class App extends Component {
           })}
       </div>
       );
+      style.backgroundColor = 'red';
+    }
+
+    let classes = [];
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1){
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button 
           onClick={this.togglePersonHandler}
           style={style}>Toggle Persons</button>
