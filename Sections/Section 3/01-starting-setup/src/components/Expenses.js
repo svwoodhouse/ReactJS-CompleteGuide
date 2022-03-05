de99cdf5633cc00ce6264ae1963b3bfc83ec15f5
Sydnee <svwoodhouse@gmail.com>
2022-03-04 19:13:49 -0500

    Created Card Wrapper component

--- Sections/Section 3/01-starting-setup/src/components/Expenses.js
+++ Sections/Section 3/01-starting-setup/src/components/Expenses.js
@@ -1,15 +1,16 @@
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem'
+import Card from './Card'
 
 function Expenses (props) {
 
     return(
-        <div className='expenses'>
+        <Card className='expenses'>
             <ExpenseItem title={props[0].title} amount={props[0].amount} date={props[0].date}></ExpenseItem>
             <ExpenseItem title={props[1].title} amount={props[1].amount} date={props[1].date}></ExpenseItem>
             <ExpenseItem title={props[2].title} amount={props[2].amount} date={props[2].date}></ExpenseItem>
             <ExpenseItem title={props[3].title} amount={props[3].amount} date={props[3].date}></ExpenseItem>
-        </div>
+        </Card>
     )
 }
 
