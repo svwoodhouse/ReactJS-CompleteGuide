455b897dd68a6377952d5355a5032d0a07b8c1b0
Sydnee <svwoodhouse@gmail.com>
2022-11-20 00:54:41 -0500

    Added Expense Filter

---
+++ Sections/Section 3/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import './ExpenseForm.css'
+const ExpenseForm = (props) => {
+    const[enteredTitle, setEnteredTitle] = useState('')
+    const[enteredAmount, setEnteredAmount] = useState('')
+    const[enteredDate, setEnteredDate] = useState('')
+
+   // const[userInput, setUserInput] = useState({
+   //    enteredTitle: '',
+   //    enteredAmount: '',
+   //    enteredDate: ''
+   //})
+
+    const titleChangeHandler = (event) => {
+       setEnteredTitle(event.target.value)
+      // setUserInput({
+          // ...userInput,
+          // title:event.target.value
+       // })
+       // setUserInput((prevState) => { return {...prevState, enteredTitle: event.target.value}})
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+        //setUserInput({
+        //    ...userInput,
+        //    amount:event.target.value
+        //})
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value)
+        // setUserInput({
+        //  ...userInput,
+        //  date:event.target.value
+        //})
+    }
+    const submitHandler = (event) => {
+    // By default, forms update the page onSubmit so to prevent that from 
+    // happenning we use the preventDefault() function
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle, 
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        }
+        props.onSaveExpenseData(expenseData)
+        setEnteredTitle('')
+        setEnteredAmount('')
+        setEnteredDate('')
+
+    }
+
+    return ( 
+        <form onSubmit={submitHandler}>
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
+                    <label>Title</label>
+                    <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
+                </div>
+                <div className='new-expense__control'>
+                    <label>Amount</label>
+                    <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
+                </div>
+                <div className='new-expense__control'>
+                    <label>Date</label>
+                    <input type='date' min="2022-01-01" max="2024-12-31" value={enteredDate} onChange={dateChangeHandler}/>
+                </div>
+            </div>
+            <div className='new-expense__actions'>
+                <button type='submit'>Add Expense</button>
+            </div>
+        </form>
+    )
+}
+
+export default ExpenseForm
