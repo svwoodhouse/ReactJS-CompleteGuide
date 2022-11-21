6a9af3ae8068b5b19654a8940e936e2f76293a2f
Sydnee <svwoodhouse@gmail.com>
2022-11-20 23:39:22 -0500

    Added ChartBar and Chart component

--- Sections/Section 3/01-starting-setup/src/components/Expenses/Expenses.js
+++ Sections/Section 3/01-starting-setup/src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import './Expenses.css'
 import ExpensesList from './ExpensesList'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
+import ExpensesChart from './ExpensesChart'
 import { useState } from 'react'
 
 const Expenses = (props) => {
@@ -19,6 +20,7 @@ const Expenses = (props) => {
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                <ExpensesChart expenses={filteredExpenses}/>
                 <ExpensesList items = {filteredExpenses}/>
             </Card>
         </div>
