20475e2b599da733720a252637642e7a9d5ac8ab
Sydnee <svwoodhouse@gmail.com>
2023-01-03 23:16:29 -0500

    Added loading logic

--- Sections/Section 14/01-starting-project/src/App.js
+++ Sections/Section 14/01-starting-project/src/App.js
@@ -20,19 +20,35 @@ function App() {
   // ];
 
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   async function fetchMoviesHandler() {
-    const response = await fetch('https://swapi.py4e.com/api/films')
-    const data = await response.json();
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
+    setIsLoading(true)
+    setError(null)
+    try {
+      const response = await fetch('https://swapi.py4e.com/api/films')
+
+      if(!response.ok){
+        throw new Error('Something went wrong!')
+      }
+
+      const data = await response.json();
+        const transformedMovies = data.results.map(movieData => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date
+          }
+        })
       setMovies(transformedMovies)
+      setIsLoading(false)
+    }
+    catch (error) {
+      setError(error.message)
+    }
+    setIsLoading(false)
   }
 
   return (
@@ -41,7 +57,10 @@ function App() {
         <button onClick={()=> {fetchMoviesHandler()}}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>No movies found</p>}
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
