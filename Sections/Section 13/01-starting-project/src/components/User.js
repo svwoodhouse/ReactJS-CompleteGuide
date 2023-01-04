e77c28e9127c37374f5e0e337a448e6a0292587b
Sydnee <svwoodhouse@gmail.com>
2023-01-03 21:30:31 -0500

    added new section

--- Sections/Section 13/01-starting-project/src/components/User.js
+++ Sections/Section 13/01-starting-project/src/components/User.js
@@ -1,7 +1,17 @@
 import classes from './User.module.css';
+import { Component } from 'react';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  componentWillUnmount(){
+    console.log('User will Unmount')
+  }
+  render(){
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+// Functional base component
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
