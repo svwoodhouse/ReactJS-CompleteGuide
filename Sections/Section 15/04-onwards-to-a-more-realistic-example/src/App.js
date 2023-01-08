caa6e653e00e180cc4e4a350c4e08b9b83d9251e
Sydnee <svwoodhouse@gmail.com>
2023-01-08 11:00:49 -0500

    Added generic react hook for http requests

--- Sections/Section 15/04-onwards-to-a-more-realistic-example/src/App.js
+++ Sections/Section 15/04-onwards-to-a-more-realistic-example/src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
-import useData from './hooks/use-data';
+// import useData from './hooks/use-data';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const { isLoading, taskAddHandler, tasks, fetchTasks, error } = useData()
+  // For the useData customer React Hook example
+  // const { isLoading, taskAddHandler, tasks, fetchTasks, error } = useData()
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: 'https://react-http-e4c37-default-rtdb.firebaseio.com/tasks.json' },
+      transformTasks
+    );
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
 
   return (
     <React.Fragment>
