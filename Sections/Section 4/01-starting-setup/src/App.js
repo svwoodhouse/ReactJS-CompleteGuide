a968cdd2ac1c52b54548fbe826652e27f35c47d0
Sydnee Woodhouse <svwoodhouse@gmail.com>
2023-07-18 23:04:38 -0400

    added changes

--- Sections/Section 4/01-starting-setup/src/App.js
+++ Sections/Section 4/01-starting-setup/src/App.js
@@ -2,6 +2,7 @@ import keyConceptsImage from './assets/images/key-concepts.png';
 import componentsImage from './assets/images/components.png';
 import stateImage from './assets/images/state.png';
 import eventsImage from './assets/images/events.png';
+import CardContent from './components/CardContent';
 
 const concepts = [
   {
@@ -33,11 +34,9 @@ function App() {
         <p>Selected key React concepts you should know about</p>
       </header>
       <ul id="concepts">
-        <li className="concept">
-          <img src="TODO: IMAGE" alt="TODO: TITLE" />
-          <h2>TODO: TITLE</h2>
-          <p>TODO: DESCRIPTION</p>
-        </li>
+        { concepts.map(item=> 
+          <CardContent title={item.title} description={item.description} image={item.image}/>
+        )}
       </ul>
     </div>
   );
