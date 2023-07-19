d97c610c97c8452e5dfaaf12bd25ecfbd67fe301
Sydnee Woodhouse <svwoodhouse@gmail.com>
2023-07-18 22:40:17 -0400

    removed un needed files

--- Sections/Section 3/01-starting-setup/src/components/Expenses/ExpenseDate.js
+++ Sections/Section 3/01-starting-setup/src/components/Expenses/ExpenseDate.js
@@ -1,8 +1,8 @@
 import './ExpenseDate.css'
-const ExpenseDate = (props) => {
-    const month = props.date.toLocaleString('en-US', {month: 'long'});
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.date.getFullYear();
+const ExpenseDate = ({date}) => {
+    const month = date.toLocaleString('en-US', {month: 'long'});
+    const day = date.toLocaleString('en-US', {day: '2-digit'});
+    const year = date.getFullYear();
 
     return (
     <div className="expense-date">
